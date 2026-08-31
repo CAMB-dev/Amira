0b947fca7e7e908f0143f6c0227e35f8345eb177
camb <cambdev@outlook.com>
2026-08-31 16:34:13 +0800

    build(client): add WinUI client shell

---
+++ clients/Amira.Client.WinUI/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+using Microsoft.UI.Xaml;
+
+namespace Amira.Client.WinUI;
+
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Title = "Amira";
+    }
+}
